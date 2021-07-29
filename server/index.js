4d0deadcb730ed21f796f3280389205ab2405ae5
Abhiramborige <abhiramborige@gmail.com>
2021-07-29 20:00:53 +0530

    💥 hosted

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const { scrape_api } = require("./wiki_api");
 const { scrape_Product } = require("./scraper");
 
