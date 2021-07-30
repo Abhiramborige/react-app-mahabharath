a5704191e473272f027e2cb6a0c09ed6e83d3ab0
Abhiramborige <abhiramborige@gmail.com>
2021-07-30 15:40:45 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 const { scrape_api } = require("./wiki_api");
 const { scrape_Product } = require("./scraper");
 
@@ -12,12 +11,12 @@ app.use(function (req, res, next) {
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
 // https://www.urlencoder.io/ and https://www.urldecoder.io/
-function encode(name){
-  return encodeURIComponent(name)
+function encode(name) {
+  return encodeURIComponent(name);
 }
 
 app.get("/:name", async (req, res) => {
-  let name=encode(req.params.name)
+  let name = encode(req.params.name);
   const result = await scrape_Product(
     `https://en.m.wikipedia.org/wiki/${name}`
   );
@@ -26,7 +25,7 @@ app.get("/:name", async (req, res) => {
 });
 
 app.get("/get/:name", async (req, res) => {
-  let name=encode(req.params.name)
+  let name = encode(req.params.name);
   const info = await scrape_api(
     `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&titles=${name}&explaintext=1&formatversion=2&format=json`
   );
@@ -38,17 +37,17 @@ app.get("/get/:name", async (req, res) => {
   res.send(
     JSON.stringify(
       {
-        "name":info.query.pages[0].title,
-        "info": info.query.pages[0].extract,
-        "img": img.query.pages[0].original.source,
-        "desc": img.query.pages[0].terms.description[0],
+        name: info.query.pages[0].title,
+        info: info.query.pages[0].extract,
+        img: img.query.pages[0].original.source,
+        desc: img.query.pages[0].terms.description[0],
       },
       null,
       4
     )
   );
 });
-
+const port = process.env.PORT || "8000";
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
