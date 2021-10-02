dad3bab5040041b5eb2725044256f82ce17057db
Alberto Herrera <albertoherreravargas@gmail.com>
2021-10-02 21:31:06 +0200

    Adding new component to display the login and signup page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Loader from "./Components/loader";
+import LogComponent from "./OtherPages/login";
 
 const MainApp = lazy(() => import("./MainApp"));
 const About = lazy(() => import("./OtherPages/About"));
@@ -25,6 +26,12 @@ const App = () => {
         </Route>
         <Route path="/about">
           <About></About>
+        </Route>
+        <Route path="/login">
+          <LogComponent isLogin={true} onClickCallback={() => { }} />
+        </Route>
+        <Route path="/signup">
+          <LogComponent isLogin={false} onClickCallback={() => { }} />
         </Route>
       </Switch>
     </Suspense>
